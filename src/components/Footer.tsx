c1a383fd5e1aa2ff9986d4db79a6cb477c068393
Vasanth Saravanan <vasanths991@gmail.com>
2025-05-24 16:55:32 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -110,11 +110,11 @@ const Footer = () => {
             <div className="mt-6 space-y-2">
               <div className="flex items-center space-x-2 text-gray-400 text-sm">
                 <Phone className="h-4 w-4" />
-                <span>+1 (555) 123-4567</span>
+                <span></span>
               </div>
               <div className="flex items-center space-x-2 text-gray-400 text-sm">
                 <MapPin className="h-4 w-4" />
-                <span>San Francisco, CA</span>
+                <span>San Chennai, India>
               </div>
             </div>
           </div>
